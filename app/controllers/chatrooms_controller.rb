45d443ed8de9945a2861445373eaeee96a54d9d7
Denver Stove <dtstove@gmail.com>
2018-08-21 20:20:16 +1000

    Thrashing it out

---
+++ app/controllers/chatrooms_controller.rb
@@ -0,0 +1,30 @@
+class ChatroomsController < ApplicationController
+
+  def index
+    @chatrooms = Chatroom.all
+  end
+
+  def show
+    @chatroom = Chatroom.find(params[:id])
+  end
+
+  def new
+    @chatroom = Chatroom.new
+  end
+
+  def create
+    @chatroom = Chatroom.new(chatroom_params)
+    if @chatroom.save
+      redirect_to
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def chatroom_params
+    params.require(:chatroom).permit(:topic)
+  end
+
+end
---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: "chatrooms#index"
+  resources :users do
+    resources :messages
+  end
+  resources :chatrooms
+end
